038ffe2995b4322475f252a0af40c73bf1804890
rajaman2k <88160864+rajaman2k@users.noreply.github.com>
2021-11-22 09:08:20 +0530

    Add files via upload

---
+++ lnblist.py
@@ -0,0 +1,22 @@
+print("Enter elements for list 1, seperated by a space:")
+a=input()
+L1=list(a.split(" "))
+L1=list(map(int,L1))
+
+print("Enter elements for list 2, seperated by a space:")
+a=input()
+L2=list(a.split(" "))
+L2=list(map(int,L2))
+
+L3=[]
+
+for i in range(1,len(L1),2):
+    x=L1[i]
+    L3.append(x)
+
+for i in range(0,len(L2),2):
+    x=L2[i]
+    L3.append(x)
+
+print(L3)        
+
